c14c5b3251edf6ee6b7126d9858fc3b19dde6083
Job Vranish <github@jobvranish.com>
2023-06-17 12:34:57 -0400

    Cleaned up webrtc diagnostics, and added some documentation

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 import { runTests } from "./test-runner.js";
-// import "./signaling-service/signaling-tests.js";
-// import "./webrtc-tests.js";
-// import "./signaling-service/identity-tests.js";
+import "../lib/signaling-service/signaling-tests.js";
+import "../lib/webrtc/webrtc-tests.js";
+import "../lib/crypto/identity-tests.js";
 import "../lib/networking/networking-test.js";
 
 
